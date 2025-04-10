0aaaacdd020497c005144fe16c0af960a57dc5a1
José Ángel Olmedo Guevara <joseangelolmedoguevara@ciencias.unam.mx>
2025-04-10 14:35:28 -0600

    Continuando

--- MergeSort/MergeSort.java
+++ MergeSort/MergeSort.java
@@ -1,3 +1,28 @@
 public class MergeSort{
-	System.out.println("Hola Papus");
+
+	public static void mergeSort(int[] arregloDeNumerosAleatorios){
+
+		int longitudOriginal = arregloDeNumerosAleatorios.length; //Será util tener la longitud del arreglo de entrada
+		
+		if(longitudOriginal < 2){ //Si la longitud es de 1 ó 0, entonces ya está ordenado
+			return; //Terminamos la pila de ejecución
+		}
+
+		int indiceDeEnmedio = arregloDeNumerosAleatorio / 2; //Si el arreglo es de longitud impar truncará el entero
+		int[] mitadIzquierda = new int[indiceDeEnmedio]; //El arreglo tendrá la longitud de la mitad de la longitud original
+		int[] mitadDerecha = new int[longitudOriginal - indiceDeEnmedio]; //En caso de que la longitudOriginal sea impar
+
+		/*Procedemos a rellenar el arreglo mitadIzquierda*/
+		for(int contador = 0; contador < indiceDeEnmedio; contador++) {
+			mitadIzquierda[contador] = arregloDeNumerosAleatorios[contador];
+		}
+
+		/*Procedemos a rellenar el arreglo mitadDerecha*/
+		for(int contador = indiceDeEnmedio; contador < longitudOriginal; contador++){
+			mitadDerecha[contador - indiceDeEnmedio] = arregloDeNumerosAleatorios[contador];
+		}
+
+
+	}
+	
 }
