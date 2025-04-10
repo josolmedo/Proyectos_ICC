d1353fae6e290fe71e0346ddb584eecc70f3b182
José Olmedo <116040147+josolmedo@users.noreply.github.com>
2025-04-10 14:17:20 -0600

    Create Main.java

---
+++ MergeSort/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String[] args){
+
+		int[] arregloDeNumerosAleatorios = GenerarArregloDeEnterosAleatorios.generar(20);
+		System.out.println("\nAntes de ordenar: \n");
+		ImprimirArreglo.imprimir(arregloDeNumerosAleatorios);
+
+		System.out.println("\nDespues de ordenar: \n");
+
+
+
+	}
+}
